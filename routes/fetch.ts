60e461bd2e56f143a4db18703a977517c4c6b13d
snowypy <git@snowyjs.lol>
2025-01-12 18:15:21 +0000

    Add fetchall route.

--- routes/fetch.ts
+++ routes/fetch.ts
@@ -1,18 +1,38 @@
 import path from "path";
 import express from "express";
+import User from "../model/User";
 
 const router = express.Router();
 
 router.get('/:user/:image', (req, res) => {
-    const user = req.params.user;
+    const username = req.params.user;
     const image = req.params.image;
-    const imagePath = path.join(__dirname, '..', 'uploads', user, image);
+    const imagePath = path.join(__dirname, '..', 'uploads', username, image);
 
     res.sendFile(imagePath, (err) => {
         if (err) {
             res.status(404).end();
         }
     });
+});
+
+router.post('/getall', async (req, res) => {
+    const username = req.body.username;
+    const apiKey = req.body.apiKey;
+
+    const user = await User.findOne(username);
+
+    if (!user) {
+        res.status(404).json({error: 'User not found'});
+        return;
+    }
+
+    if (apiKey !== user.apiKey) {
+        res.status(403).json({error: 'Access denied'});
+        return;
+    }
+
+    res.json({files: user.uploadedFiles});
 });
 
 export default router;
