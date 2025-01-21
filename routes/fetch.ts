efb91319f5d97f192d07401fb0a96bcbb60685b0
Cigan <git@snowyjs.lol>
2025-01-21 14:41:55 +0000

    implemented a demo fetch route for file info.

--- routes/fetch.ts
+++ routes/fetch.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import User from "../model/User";
+import getFileInfo from "../utils/filesize"
 
 const router = express.Router();
 
@@ -15,6 +16,20 @@ router.get('/:user/:image', (req, res) => {
         }
     });
 });
+
+router.get('/info/:user/:image', (req, res) => {
+    const user = req.params.user;
+    const image = req.params.image
+    const path = path.join(___dirname, '..', 'uploads', username, image)
+    const fileSize = getFileInfo(path)
+
+    if (!fileSize) {
+        res.status(501).json({servererror: 'Could not grab file size.'})
+    }
+
+    res.json({fileSize})
+
+})
 
 router.post('/getall', async (req, res) => {
     const username = req.body.username;
